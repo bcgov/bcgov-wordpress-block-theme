235d6e31dfc4b6c92a59de2ac104c56aa263d058
Nate King <codewise.nate@gmail.com>
2022-07-13 15:38:07 -0700

    DESCW-467 Admin theme features documentation page Page/admin link outlining templates + parts, will add instructions here

---
+++ inc/core/templates/theme-info-page.php
@@ -0,0 +1,42 @@
+
+<h1 class="is-medium is-sub">BCGov Block Theme Core Features</h1>
+
+<div class="auth-app-card card">
+<h2>Customisable Templates</h2>
+<ul>
+	<li><span class="dashicons dashicons-yes"></span> Page: displays a single page with the standard layout</li>
+	<li><span class="dashicons dashicons-yes"></span> Page – no title: allows for custom banners</li>
+	<li><span class="dashicons dashicons-yes"></span> Blank: no default header, content or footer – for use in creating custom landing pages</li>
+	<li><span class="dashicons dashicons-yes"></span> Search: display results page</li>
+	<li><span class="dashicons dashicons-yes"></span> 404: displays when no content is found, provides customisable 404 information</li>
+</ul>
+<p>Templates Instructions TBD</p>
+</div>
+
+<div class="auth-app-card card">
+<h2>Customisable Template Parts</h2>
+<ul>
+	<li><span class="dashicons dashicons-yes"></span> Header – Standard Blue: adds typical BC Gov blue header with site logo, title, navigation and search</li>
+	<li><span class="dashicons dashicons-yes"></span> Navigation: adds standard menu navigation to the header</li>
+	<li><span class="dashicons dashicons-yes"></span> Search Field: adds standard search field to the header</li>
+	<li><span class="dashicons dashicons-yes"></span> Breadcrumb Nav: adds the breadcrumb navigation, <a href="<?php echo admin_url( 'plugins.php' ); ?>" rel="noreferrer">requires AIOSEO plugin activation</a></li>
+	<li><span class="dashicons dashicons-yes"></span> Footer: adds typical BC Gov blue footer</li>
+</ul>
+<p>Template Parts Instructions TBD</p>
+</div>
+
+<div class="auth-app-card card">
+<h2>General Block Patterns</h2>
+<ul>
+	<li><span class="dashicons dashicons-yes"></span> ...coming soon</li>
+</ul>
+<p>Block Patterns Instructions TBD</p>
+</div>
+
+<div class="auth-app-card card">
+<h2>Site Specific Block Patterns</h2>
+<ul>
+	<li><span class="dashicons dashicons-yes"></span> ...coming soon</li>
+</ul>
+<p>Site Specific Instructions TBD</p>
+</div>
