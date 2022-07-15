8aebfd0c259d5648bd43f4d46e9b0c1a3c30ea61
Nate King <codewise.nate@gmail.com>
2022-07-15 11:53:22 -0700

    DESCW-463 Setting up Block Patterns

---
+++ inc/core/patterns/footer-default.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Default footer block pattern
+ */
+return array(
+	'title'	  => __( 'Default footer', 'bcgov_blocks_theme' ),
+	'categories' => array( 'bcgov-blocks-theme-footers' ),
+	'blockTypes' => array( 'core/template-part/footer' ),
+	'content'	=> 'WP template content goes here',
+);
---
+++ inc/core/patterns/page-front-cleanbc.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * CleanBC full front page block pattern
+ */
+return array(
+	'title'	  => __( 'CleanBC Front page', 'bcgov_blocks_theme' ),
+	'categories' => array( 'bcgov-blocks-theme-pages-cleanbc' ),
+	'content'	=> 'WP template content goes here',
+);
