af28f4c16c62854b4fa2dea8060b5e36f8db5bf0
Nate King <codewise.nate@gmail.com>
2022-08-02 19:09:25 -0700

    DESCW-511 Added basic takeover menu styling Including template part

--- dist/admin.asset.php
+++ dist/admin.asset.php
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 <?php return array('dependencies' => array('lodash', 'wp-blockEditor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n'), 'version' => '6a9b2b7230f4657fa32c');
+=======
+<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n'), 'version' => '74c32d02d4d8567aec64');
+>>>>>>> DESCW-511 Added basic takeover menu styling
