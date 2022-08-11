0e12b348d71fd458ac4dfa246b3b4ed275ad54f7
Nate King <codewise.nate@gmail.com>
2022-08-10 19:36:34 -0700

    DESCW-514 Cleaned up categories & patterns

--- inc/core/patterns/header-default.php
+++ inc/core/patterns/header-default.php
@@ -11,7 +11,7 @@ namespace Bcgov\Theme\Block;
  */
 return array(
 	'title'      => __( 'Default header', 'bcgov_blocks_theme' ),
-	'categories' => array( 'bcgov-blocks-theme-headers' ),
+	'categories' => array( 'bcgov-blocks-theme-header-footer' ),
 	'blockTypes' => array( 'core/template-part/header' ),
 	'content'    => '<!-- wp:group {"style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}},"spacing":{"padding":{"top":"0px","right":"65px","bottom":"0px","left":"65px"}}},"borderColor":"primary-brand-alt","backgroundColor":"primary-brand","textColor":"white","className":"bcgov-header-group","layout":{"inherit":true}} -->
 	<div class="wp-block-group bcgov-header-group has-border-color has-primary-brand-alt-border-color has-white-color has-primary-brand-background-color has-text-color has-background has-link-color" style="padding-top:0px;padding-right:65px;padding-bottom:0px;padding-left:65px">
