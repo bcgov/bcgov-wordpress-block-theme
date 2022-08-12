119ebb56061b70eacb0a7c5358b21c985e56f94f
Nate King <codewise.nate@gmail.com>
2022-08-12 11:26:13 -0700

    DESCW-514 Info banner + deployment fix Removed assets + styles from gitattributes file

--- dist/admin.asset.php
+++ dist/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n'), 'version' => '58bc52a564b18eaed65c');
+<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n'), 'version' => '8147a943e655feb6e3a3');
