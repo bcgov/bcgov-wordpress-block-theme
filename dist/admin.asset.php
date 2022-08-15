391517b7bcbcc2903c9a5622123c4d125c5b1258
Nate King <codewise.nate@gmail.com>
2022-08-15 08:55:07 -0700

    DESCW-514 Add default BCGov logo SVG

--- dist/admin.asset.php
+++ dist/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n'), 'version' => '708fb1ceacd29784f3b7');
+<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n'), 'version' => 'd0666fb5bc16efafacbe');
