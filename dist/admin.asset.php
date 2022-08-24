db14ec47f407929c695b18a952d1aab1e13147f2
Nate King <codewise.nate@gmail.com>
2022-08-23 18:38:54 -0700

    DESCW-534 Checklist + changelog Includes previous PR updates

--- dist/admin.asset.php
+++ dist/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n'), 'version' => 'eff27ed9048216e555b0');
+<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n'), 'version' => '22991662f32a98e72bde');
