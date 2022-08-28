be623a26826177cfaee000f5f23eedcc51906a1e
Nate King <codewise.nate@gmail.com>
2022-08-27 20:24:25 -0700

    DESCW-534 Better header handling

--- dist/admin.asset.php
+++ dist/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n'), 'version' => 'c533d35b1908d82b2088');
+<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n'), 'version' => '569711600e986542939d');
