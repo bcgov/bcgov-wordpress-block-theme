4724d2d1d72220f9f4148600009d79282d7b475d
Nate King <codewise.nate@gmail.com>
2022-08-28 12:37:11 -0700

    DESCW-534 Default navigation fix

--- dist/admin.asset.php
+++ dist/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n'), 'version' => '569711600e986542939d');
+<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n'), 'version' => '189179bb48a3a682ca89');
--- src/scripts/public/ribbon-navigation.js
+++ src/scripts/public/ribbon-navigation.js
@@ -9,42 +9,51 @@ const domReady = () => {
 		 * Check for menu items being out of viewport and class is needed.
 		 */
 		function doBoundsCheck(t) {
-			const container = t.closest('ul');
-			const childContainer = container.querySelector('ul');
+			setTimeout(function() {
+				const container = t;
+				const childContainer = container.querySelector('ul');
 
-			let bounding = null;
-			const parentBounding = container.getBoundingClientRect();
+				let bounding = null;
+				const parentBounding = container.getBoundingClientRect();
 
-			if (null !== childContainer) {
-				bounding = childContainer.getBoundingClientRect();
-			}
-			if (
-				null !== bounding &&
-				bounding.right >
-					(window.innerWidth || document.documentElement.clientWidth)
-			) {
 				if (null !== childContainer) {
-					childContainer.classList.add('is-offscreen');
-					childContainer.style.left =
-						'calc(' + parentBounding.width + 'px - 195%)';
-					childContainer.style.right = 'auto';
-					childContainer.style.top = '50%';
-					childContainer.style.position = 'absolute';
+					bounding = childContainer.getBoundingClientRect();
 				}
-			}
+				if (
+					null !== bounding &&
+					bounding.right >
+						(window.innerWidth ||
+							document.documentElement.clientWidth)
+				) {
+					if (null !== childContainer) {
+						childContainer.classList.add('is-offscreen');
+						childContainer.style.left =
+							'calc(' + parentBounding.width + 'px - 185%)';
+						childContainer.style.right = 'auto';
+						childContainer.style.top = '20%';
+						childContainer.style.position = 'absolute';
+					}
+				}
+			}, 0);
 		}
 
 		if (document.querySelectorAll('li.wp-block-navigation-item').length) {
 			const elem = document.querySelectorAll(
 				'li.wp-block-navigation-item'
 			);
+
 			elem.forEach((i) => {
-				i.addEventListener('mousemove', (e) => {
-					doBoundsCheck(e.target);
-				});
-				i.addEventListener('keypress', (e) => {
-					doBoundsCheck(e.target);
-				});
+				i.addEventListener(
+					'pointerover',
+					(e) => {
+						let target = e.target;
+						if (target.tagName !== 'LI') {
+							target = target.closest('li');
+						}
+						doBoundsCheck(target);
+					},
+					{ capture: false }
+				);
 			});
 		}
 	}
