a3a507da3dccb5155ae03d0c0106e6626fdb0d31
Nate King <codewise.nate@gmail.com>
2022-08-29 07:06:46 -0700

    DESCW-534 Columns constaint filter

--- dist/admin.asset.php
+++ dist/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n'), 'version' => 'd4f602c9553c0bbb78e4');
+<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n'), 'version' => '6274f1772fcf75966a04');
