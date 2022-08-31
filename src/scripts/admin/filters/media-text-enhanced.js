5d1d48fd38f10cbfddf739eb697bb7474ffedc73
Nate King <codewise.nate@gmail.com>
2022-08-31 11:33:17 -0700

    DESCW-534 Fixed Media Image clipping

--- src/scripts/admin/filters/media-text-enhanced.js
+++ src/scripts/admin/filters/media-text-enhanced.js
@@ -4,7 +4,13 @@ registerBlockStyle('core/media-text', {
 	name: 'dropshadow',
 	label: 'Drop shadow',
 });
+
 registerBlockStyle('core/media-text', {
 	name: 'square',
 	label: 'Square',
+});
+
+registerBlockStyle('core/media-text', {
+	name: 'no-clip',
+	label: 'Full Image',
 });
