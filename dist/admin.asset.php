f4ea8d65a97a9791b6ce078e3acbc053b8c8ccae
Nate King <codewise.nate@gmail.com>
2022-08-31 13:03:01 -0700

    DESCW-534 Breadcrumb nav placement after banner

--- dist/admin.asset.php
+++ dist/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n'), 'version' => '058a4dea686f7ce04906');
+<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n'), 'version' => 'ac25914c767d9fd449f3');
--- inc/core/patterns/general-breadcrumb-nav.php
+++ inc/core/patterns/general-breadcrumb-nav.php
@@ -12,7 +12,7 @@ namespace Bcgov\Theme\Block;
 return [
 	'title'      => __( 'Breadcrumb Navigation', 'bcgov_blocks_theme' ),
 	'categories' => [ 'bcgov-blocks-theme-general' ],
-	'content'    => '<!-- wp:group {"tagName":"section","style":{"spacing":{"padding":{"top":"1rem","right":"1rem","bottom":"1rem","left":"1rem"}}},"layout":{"inherit":false,"contentSize":"1440px"},"fontSize":"extra-small"} -->
-	<section class="wp-block-group has-extra-small-font-size"><!-- wp:aioseo/breadcrumbs /--></section>
+	'content'    => '<!-- wp:group {"tagName":"section"},"layout":{"inherit":false,"contentSize":"1440px"},"fontSize":"extra-small"} -->
+	<section class="wp-block-group has-extra-small-font-size" style="><!-- wp:aioseo/breadcrumbs /--></section>
 	<!-- /wp:group -->',
 ];
