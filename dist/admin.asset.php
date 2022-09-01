d4c74091841ae87e8d0b2ab5d6930af53c2cc87f
Nate King <codewise.nate@gmail.com>
2022-09-01 08:55:20 -0700

    DESCW-534 Navigation styles fix

--- dist/admin.asset.php
+++ dist/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n'), 'version' => '83294823bca975584d12');
+<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n'), 'version' => '361e15fd2b3a52e0ad66');
