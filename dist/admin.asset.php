0db0fb415d7d46072de4518a3d9016f5318a86dc
Nate King <codewise.nate@gmail.com>
2022-09-01 08:38:31 -0700

    DESCW-534 In-page styles

--- dist/admin.asset.php
+++ dist/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n'), 'version' => 'ac25914c767d9fd449f3');
+<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n'), 'version' => '83294823bca975584d12');
