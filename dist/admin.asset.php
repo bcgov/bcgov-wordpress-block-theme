fd943b30592a7025adb1de67d1380f7647b66769
Nate King <codewise.nate@gmail.com>
2022-09-01 14:58:25 -0700

    DESCW-534 Block template parts Area fix + button styles

--- dist/admin.asset.php
+++ dist/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n'), 'version' => 'b4625938493a25c0c3b7');
+<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n'), 'version' => 'eaf348ade95db61c4690');
--- dist/public.asset.php
+++ dist/public.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => '29a436cbb2c46ebaac95');
+<?php return array('dependencies' => array(), 'version' => '1155ab148d94b82c9d88');
