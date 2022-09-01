9b781bd149d03d9e9ececcd4a327e0264d913de4
Nate King <codewise.nate@gmail.com>
2022-09-01 09:07:33 -0700

    DESCW-534 Breadcrumb errant style attribute fix

--- inc/core/patterns/general-breadcrumb-nav.php
+++ inc/core/patterns/general-breadcrumb-nav.php
@@ -13,6 +13,6 @@ return [
 	'title'      => __( 'Breadcrumb Navigation', 'bcgov_blocks_theme' ),
 	'categories' => [ 'bcgov-blocks-theme-general' ],
 	'content'    => '<!-- wp:group {"tagName":"section"},"layout":{"inherit":false,"contentSize":"1440px"},"fontSize":"extra-small"} -->
-	<section class="wp-block-group has-extra-small-font-size" style="><!-- wp:aioseo/breadcrumbs /--></section>
+	<section class="wp-block-group has-extra-small-font-size"><!-- wp:aioseo/breadcrumbs /--></section>
 	<!-- /wp:group -->',
 ];
