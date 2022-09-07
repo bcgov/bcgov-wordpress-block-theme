4d5aa0f04b6bd7df0806bda3eb96385c5b1f1b81
Nate King <codewise.nate@gmail.com>
2022-09-07 13:57:39 -0700

    DESCW-534 Added core/button aria-label a11y text

--- dist/admin.asset.php
+++ dist/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n'), 'version' => '295046ba33c3e456b814');
+<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n'), 'version' => '825c9cb54c8f790509fc');
