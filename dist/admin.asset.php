58309a2a4afbaec548b0bdb1968a65190e999e0c
Nate King <codewise.nate@gmail.com>
2022-09-13 11:20:02 -0700

    DESCW-534 Rolling back gitignore

--- dist/admin.asset.php
+++ dist/admin.asset.php
@@ -1,1 +1,2 @@
 <?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n'), 'version' => '966d0ea62e90c6e730cc');
+
--- dist/public.asset.php
+++ dist/public.asset.php
@@ -1,1 +1,2 @@
 <?php return array('dependencies' => array(), 'version' => '7eefeb95b510d8d36840');
+
