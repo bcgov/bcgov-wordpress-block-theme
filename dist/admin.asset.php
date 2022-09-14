cd7b0bee64dca63b1f4a46684135cebe34a342ac
Nate King <codewise.nate@gmail.com>
2022-09-13 20:54:05 -0700

    DESCW-598 Home down arrow

--- dist/admin.asset.php
+++ dist/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n'), 'version' => '39b80d00d62db474e0c9');
+<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n'), 'version' => '63c77c2b178bb9537857');
