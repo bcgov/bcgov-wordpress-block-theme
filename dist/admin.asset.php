f4f6123e2488953c8778de59cce6b2e7edac6caf
Nate King <codewise.nate@gmail.com>
2022-09-15 11:17:04 -0700

    DESCW-601 version 1.0.0 tag + mobile nav fix

--- dist/admin.asset.php
+++ dist/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n'), 'version' => 'f6495fee8020e046e761');
+<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n'), 'version' => '86569e232f8f26460ca7');
--- dist/public.asset.php
+++ dist/public.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => '8fc5404cce1d42bb8347');
+<?php return array('dependencies' => array(), 'version' => '85df2fabdb28aac72fc7');
