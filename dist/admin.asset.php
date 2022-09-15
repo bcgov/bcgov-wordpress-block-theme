0abcccfb1e90d3a07ed2fd59816e7afd9696ead2
Nate King <codewise.nate@gmail.com>
2022-09-15 14:32:22 -0700

    DESCW-601 general nav fix

--- dist/admin.asset.php
+++ dist/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n'), 'version' => '86569e232f8f26460ca7');
+<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n'), 'version' => '294507f8fca604f00eff');
--- dist/public.asset.php
+++ dist/public.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => '85df2fabdb28aac72fc7');
+<?php return array('dependencies' => array(), 'version' => '0375ce857023ffb31406');
