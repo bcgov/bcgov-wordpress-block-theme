1878612da8563c3c5fd3cc53e1bf8d5a50af2a38
Nate King <codewise.nate@gmail.com>
2022-09-26 08:54:50 -0700

    DESCW-598 CleanBC Home landing banner null check

--- dist/admin.asset.php
+++ dist/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n'), 'version' => '35530085e18f05cd990a');
+<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n'), 'version' => '1298774bdebc8f3cd31a');
