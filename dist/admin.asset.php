170ff3c97f6251ef96038eb558c974b14c76d2be
Nate King <codewise.nate@gmail.com>
2022-11-23 16:33:34 -0800

    DESCW-736 Language Switcher select styling Basic implementation for header and ribbon menu

--- dist/admin.asset.php
+++ dist/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n'), 'version' => 'c92d251e143450fd9189');
+<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n'), 'version' => '5af213dc292e94212e88');
