706031ec29dab43b6c5cb39330c627447823ef7f
Nate King <codewise.nate@gmail.com>
2023-07-05 16:12:01 -0700

    Added test for null href

--- dist/admin.asset.php
+++ dist/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n'), 'version' => '5fb6eba254ba11243b85');
+<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n'), 'version' => '546316876c56e7a343f2');
--- dist/public.asset.php
+++ dist/public.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => '53606905eb7e6036b4ce');
+<?php return array('dependencies' => array(), 'version' => '23038e272216a8263658');
