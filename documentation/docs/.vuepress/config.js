8d31e79fc7d64065f984db1facb26f7341147cfc
cdeakin <chris.deakin@gov.bc.ca>
2023-10-24 12:14:53 -0700

    DESCW-1558 Corrections in vuepress config file

--- documentation/docs/.vuepress/config.js
+++ documentation/docs/.vuepress/config.js
@@ -2,12 +2,11 @@ import { defaultTheme } from "@vuepress/theme-default";
 import { defineUserConfig } from "vuepress";
 
 export default defineUserConfig({
-  base: "/documentation/",
+  base: "/bcgov-wordpress-block-theme/",
   lang: "en-US",
   title: "Documentation",
   description: "Developer Documentation",
   theme: defaultTheme({
-    logo: "/images/BCID_H_rgb_pos.png",
     // sidebar array
     // all pages will use the same sidebar
     sidebar: [
