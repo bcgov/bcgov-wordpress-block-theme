bce62715c8b6b39b9ee55a530436709ea3adcd9d
mhaswell-bcgov <Michael.Haswell@gov.bc.ca>
2023-10-31 10:05:24 -0700

    Upgraded @wordpress/scripts to fix vulnerability with @babel/traverse

--- dist/admin.asset.php
+++ dist/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n'), 'version' => 'f6bf65d360ac544fc4db');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-i18n'), 'version' => '72a8722e18e2ac02c456');
