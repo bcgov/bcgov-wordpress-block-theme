7f003b3ba8fa288ecc30ea35bf22bebd42a9b2b8
Shawn Turple <shawn.turple@gov.bc.ca>
2023-11-29 18:01:39 -0800

    DESCW-1733 fix axio dependency

--- dist/admin.asset.php
+++ dist/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-i18n'), 'version' => '58b6d5672a3708f72dc5');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-i18n'), 'version' => 'aa5b0b62efe62dd56f9f');
