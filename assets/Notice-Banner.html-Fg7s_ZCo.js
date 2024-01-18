5cc1a108b7c350e3c80a8fdb16de42534633d865
codewisenate <codewisenate@users.noreply.github.com>
2024-01-18 01:43:39 +0000

    Deploying to gh-pages from  @ e9cd1f67d7f889d5878c7f9399ed413c6ed44575 🚀

---
+++ assets/Notice-Banner.html-Fg7s_ZCo.js
@@ -0,0 +1,1 @@
+const e=JSON.parse('{"key":"v-34a8b1c3","path":"/guide/setting/Notice-Banner.html","title":"Notice Banner","lang":"en-US","frontmatter":{},"headers":[],"git":{"updatedTime":1705541998000,"contributors":[{"name":"Nate King","email":"Nate.King@gov.bc.ca","commits":1}]},"filePathRelative":"guide/setting/Notice-Banner.md"}');export{e as data};
