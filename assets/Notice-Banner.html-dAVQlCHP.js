535b16a013d216fb427850392e2aa66c747661d7
codewisenate <codewisenate@users.noreply.github.com>
2024-02-15 23:15:51 +0000

    Deploying to gh-pages from  @ f9fd0d197e46199e1b8bc741641e8b8f3c7afea8 🚀

---
+++ assets/Notice-Banner.html-dAVQlCHP.js
@@ -0,0 +1,1 @@
+const e=JSON.parse('{"key":"v-34a8b1c3","path":"/guide/setting/Notice-Banner.html","title":"Notice Banner","lang":"en-US","frontmatter":{},"headers":[],"git":{"updatedTime":1708038299000,"contributors":[{"name":"Nate King","email":"Nate.King@gov.bc.ca","commits":1}]},"filePathRelative":"guide/setting/Notice-Banner.md"}');export{e as data};
